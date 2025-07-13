6d4659a099661f9ef53e7e3fa732e2f2c4438d1d
Ashutosh Sharma <ashutoshsharmaengg@live.com>
2025-07-12 19:39:10 -0700

    autogen update date 2

---
+++ 1. Autogen Basics/6. Langchain_Tools.py
@@ -0,0 +1,10 @@
+import asyncio
+from autogen_agentchat.agents import AssistantAgent
+from autogen_ext.models.openai import OpenAIChatCompletionClient
+from autogen_core.tools import FunctionTool
+import os
+from dotenv import load_dotenv
+
+from langchain_community.utilities import GoogleSerperAPIWrapper
+
+from autogen_ext.tools.http import HttpTool
