fd658d970daaab240d2de4fecfebab1c00011f89
Ashutosh Sharma <ashutoshsharmaengg@live.com>
2025-07-15 01:00:01 -0700

    structure updated

--- 1. Autogen Basics/6. Inbuilt_Tools.py
+++
@@ -1,8 +0,0 @@
-import asyncio
-from autogen_agentchat.agents import AssistantAgent
-from autogen_ext.models.openai import OpenAIChatCompletionClient
-from autogen_core.tools import FunctionTool
-import os
-from dotenv import load_dotenv
-
-from autogen_ext.tools.http import HttpTool
